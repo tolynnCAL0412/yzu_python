72505fe614948a309457c155255cffb98e52f7f3
tolynnCAL0412 <tolynn@gmail.com>
2020-04-15 21:31:12 +0800

    lesson2

--- lesson02/Case09.py
+++ lesson02/Case09.py
@@ -1,9 +1,10 @@
 # -*- coding:UTF-8 -*-
-
+import math
 # BMI 資料運算
 h = float(input('請輸入身高 : '))
 w = float(input('請輸入體重 : '))
-bmi = w / (h/100)**2
+# bmi = w / (h/100)**2
+bmi = w / math.pow(h/100, 2)
 result = 18 <= bmi <= 23
 print("身高: %.1f 體重: %.1f BMI: %.2f Result: %s" % (h, w, bmi, result))
 
