220b76cb966417c67ef560d6ccd411ba1ee0a80b
tolynnCAL0412 <tolynn@gmail.com>
2020-04-15 21:21:10 +0800

    lesson2

--- lesson02/Case06.py
+++ lesson02/Case06.py
@@ -1,8 +1,10 @@
 # -*- coding:UTF-8 -*-
 
 exchange = 29.123
-print('%09.4f' % exchange)
+print('%015.4f' % exchange)
 exchange = 129.123
-print('%09.4f' % exchange)
+print('%015.4f' % exchange)
 exchange = 9.123
-print('%09.4f' % exchange)
+print('%015.4f' % exchange)
+exchange = 987654321.123
+print('%015.4f' % exchange)
