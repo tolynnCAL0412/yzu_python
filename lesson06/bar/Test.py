2b0217b7f9c3fc89924976921e5bc576969007fe
LE-S04 <tolynn@gmail.com>
2020-05-13 20:40:11 +0800

    Lesson06

--- lesson06/bar/Test.py
+++ lesson06/bar/Test.py
@@ -7,3 +7,6 @@ print(random()) # 0 <= n < 1.0
 print(randint(1, 10)) # 1 <= n <= 10
 print(randrange(1, 10)) # 1 <= n < 10
 lan = ["Java", "Python", "C#", "Swift"]
+print(choice(lan)) # 任取一字串
+shuffle(lan) # 洗牌
+print(lan)
