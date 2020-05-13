b79d992dff2e068ceab84e6bb3d8a344acdd3aba
LE-S04 <tolynn@gmail.com>
2020-05-13 20:55:55 +0800

    Lesson06

---
+++ lesson06/WriteFile.py
@@ -0,0 +1,10 @@
+file = open('salary.txt', 'a')  # w 覆蓋, a 增加
+
+file.write('Jane, 80000\n')
+file.newlines
+file.write('Mary, 70000\n')
+file.newlines
+file.write('John, 60000\n')
+file.newlines
+
+print('OK')
