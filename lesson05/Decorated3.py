73e16109f47812c399c26e0b8b2cfa34780e3dd8
LE-S04 <tolynn@gmail.com>
2020-05-13 19:35:40 +0800

    lesson05

--- lesson05/Decorated3.py
+++ lesson05/Decorated3.py
@@ -4,7 +4,14 @@ def make_dress(func):
         func()
     return dress
 
+def make_shoes(func):
+    def dress():
+        print("穿鞋子")
+        func()
+    return dress
+
 @make_dress
+@make_shoes
 def out():
     print("我出門了")
 
