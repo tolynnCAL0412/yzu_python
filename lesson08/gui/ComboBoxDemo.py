a822df54d667599e159fb3211fd4f356829d16b1
LE-S03 <tolynn@gmail.com>
2020-05-27 21:03:51 +0800

    lesson08

--- lesson08/gui/ComboBoxDemo.py
+++ lesson08/gui/ComboBoxDemo.py
@@ -4,9 +4,10 @@ from tkinter import ttk
 win = tkinter.Tk()
 win.geometry('300x300')
 
-fruits = ['apple', 'banana', 'mango', 'melonwater']
-combo = ttk.Combobox(win, values=fruits, state='readonly')  # readonly, disabled
-combo.current(1)
+# fruits = ['apple', 'banana', 'mango', 'melonwater']
+fruits = {'apple':50, 'banana':60, 'mango':70, 'melonwater':25}
+combo = ttk.Combobox(win, values=list(fruits.keys()), state='readonly')  # readonly, disabled
+combo.current(1)    # index
 combo.pack()
 
 win.mainloop()
