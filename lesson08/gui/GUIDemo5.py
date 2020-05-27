41d94bd6d002ebcc63b62aa9b03c6d7dedaf6fa8
LE-S03 <tolynn@gmail.com>
2020-05-27 20:51:18 +0800

    lesson08

---
+++ lesson08/gui/GUIDemo5.py
@@ -0,0 +1,17 @@
+import tkinter
+from tkinter import messagebox
+
+def mytext():
+    messagebox.showinfo('Message', entry.get())
+
+win = tkinter.Tk()
+win.geometry("300x300")
+
+entry = tkinter.Entry(win, justify=tkinter.LEFT)
+entry.pack()
+
+button = tkinter.Button(win, text='Get', command=mytext)
+button.pack()
+
+
+win.mainloop()
