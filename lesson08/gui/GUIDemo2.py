a1c59db352dc1b513976e4d108c7618d9b9ad8fb
LE-S03 <tolynn@gmail.com>
2020-05-27 19:59:00 +0800

    lesson08

--- lesson08/gui/GUIDemo2.py
+++ lesson08/gui/GUIDemo2.py
@@ -4,11 +4,20 @@ win = tkinter.Tk()
 
 win.title('我的小視窗')
 
-lable = tkinter.Label(win, text='Hello')
-lable.pack(side=tkinter.LEFT)
-
+lable1 = tkinter.Label(win, text='Hello1')
 lable2 = tkinter.Label(win, text='Hello2')
-lable2.pack(side=tkinter.RIGHT)
+lable3 = tkinter.Label(win, text='Hello3')
+lable4 = tkinter.Label(win, text='Hello4')
+lable5 = tkinter.Label(win, text='Hello5')
+lable6 = tkinter.Label(win, text='Hello6')
+button1 = tkinter.Button(win, text='Button1')
+lable1.grid(column=0, row=0)
+lable2.grid(column=1, row=0)
+lable3.grid(column=2, row=0)
+lable4.grid(column=0, row=1)
+lable5.grid(column=0, row=2)
+lable6.grid(column=0, row=3)
+button1.grid(column=0, row=4)
 
 win.geometry('300x300')
 
