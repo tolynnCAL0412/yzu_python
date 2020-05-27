e6aab1fc902b90484b40334cfa7fa2f5a619549c
LE-S03 <tolynn@gmail.com>
2020-05-27 18:56:56 +0800

    lesson08

--- lesson08/ExceptDemo1.py
+++ lesson08/ExceptDemo1.py
@@ -1,3 +1,6 @@
+import sys
+import traceback
+
 x = 10
 y = int(input('請輸入數字:'))
 try:
@@ -6,3 +9,9 @@ try:
 except ZeroDivisionError as e:
     print('有錯誤發生', e)
     print(e.__class__.__name__)
+    print(e.args)
+    cl, exc, tb = sys.exc_info()
+    lastCallStack = traceback.extract_tb(tb)[-1]
+    print(lastCallStack)
+else:
+    print(z)
